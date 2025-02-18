7604095c6a479cf2fafe9236982c77e88398bb4d
Marcos <230733@utxicotepec.edu.mx>
2025-02-18 17:32:54 -0600

    Validacion de duplicidad

--- react/src/components/StudentForm.jsx
+++ react/src/components/StudentForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './StudentForm.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const StudentForm = ({ onStudentAdded }) => {
     const [student, setStudent] = useState({
@@ -19,17 +21,17 @@ const StudentForm = ({ onStudentAdded }) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
         for (const key in student) {
             if (!student[key].trim()) {
-                alert(`El campo ${key.replace('_', ' ')} no puede estar vacío.`);
+                toast.error(`El campo ${key.replace('_', ' ')} no puede estar vacío.`);
                 return;
             }
         }
-    
+
         try {
             const response = await axios.post('http://10.10.60.28:3000/api/students/insert', student);
-            alert('Student saved successfully');
+            toast.success('Student saved successfully');
             setStudent({
                 student_id: '',
                 name: '',
@@ -40,13 +42,18 @@ const StudentForm = ({ onStudentAdded }) => {
             });
             onStudentAdded(response.data.data); // Notify parent component
         } catch (error) {
+            if (error.response && error.response.data && error.response.data.data && error.response.data.data.message) {
+                toast.error(error.response.data.data.message);
+            } else {
+                toast.error('Failed to save student');
+            }
             console.error('Error saving student:', error);
-            alert('Failed to save student');
         }
     };
-    
+
     return (
         <form onSubmit={handleSubmit} className="student-form">
+            <ToastContainer />
             <div>
                 <label className='label' htmlFor="txtStudentId">Matrícula</label>
                 <input type="number" name="student_id" id="txtStudentId" value={student.student_id} onChange={handleChange} />
