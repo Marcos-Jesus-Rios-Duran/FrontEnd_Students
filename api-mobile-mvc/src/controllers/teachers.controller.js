1f57774bc571cb8eb8e006f913849f8e2b0fd7d5
Marcos Jesús Ríos Duran <230733@utxicotepec.edu.mx>
2025-02-19 10:40:03 -0600

    Update teachers.controller.js

--- api-mobile-mvc/src/controllers/teachers.controller.js
+++ api-mobile-mvc/src/controllers/teachers.controller.js
@@ -1,6 +1,15 @@
+import Joi from "joi";
 import teacherDAO from "../dao/teachers.dao.js";
 const teachersController ={};
 
+const teacherSchema = Joi.object({
+    teacher_number: Joi.number().required(),
+    name: Joi.string().required(),
+    lastname: Joi.string().required(),
+    age: Joi.number().min(18).required(),  // Asegurarse de que la edad sea mayor o igual a 18
+    career: Joi.string().required(),
+    salary: Joi.number().min(0).required() // Asegurarse de que el salario no sea negativo
+});
 
 teachersController.getAllT = (req, res) => {
     teacherDAO.getAllT()
@@ -33,23 +42,41 @@ teachersController.getOne=(req,res)=>{
 
 }
 
-teachersController.insert=(req, res)=>{
-    teacherDAO.insert(req.body)
-    .then((response)=>{
-        res.json({
-            data:{
-                message: "Teacher saved",
-                student: response
+teachersController.insert = async (req, res) => {
+    const { error } = teacherSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({
+            data: {
+                message: error.details.map(detail => detail.message)
             }
-        })
-    })
-    .catch((error)=>{
+        });
+    }
+
+    try {
+        const existingTeacher = await teacherDAO.getOne(req.body.teacher_number);
+        if (existingTeacher) {
+            return res.status(400).json({
+                data: {
+                    message: ["Teacher already exists"]
+                }
+            });
+        }
+
+        const response = await teacherDAO.insert(req.body);
         res.json({
-            data:{
-                message:error     
-        }})
-    })
-}
+            data: {
+                message: "Teacher saved",
+                teacher: response
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            data: {
+                message: [error.message]
+            }
+        });
+    }
+};
 
 teachersController.updateOne=(req, res)=>{
     teacherDAO.updateOne(req.body, req.params.teacher_number)
