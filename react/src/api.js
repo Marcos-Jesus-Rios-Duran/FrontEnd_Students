1af6a09e74d64621e2c2ac66000c94f413181fb2
Marcos Jesús Ríos Duran <230733@utxicotepec.edu.mx>
2025-02-18 23:38:03 -0600

    Update api.js

--- react/src/api.js
+++ react/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:3000/api/students'; // URL base de tu API con el prefijo
+const apiUrl = 'http://192.168.1.72:3000/api/students'; // URL base de tu API con el prefijo
 
 const api = axios.create({
     baseURL: apiUrl,
